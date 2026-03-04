de15cdf28aca57c0a53c044b2fff754da406ad33
aymen <Aymen.BenAbdallah@esprit.tn>
2026-03-05 00:42:13 +0100

    clean main

--- src/main/java/tn/spring/quiz/Repositories/ContentRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.spring.quiz.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.spring.quiz.Models.Content;
-
-public interface ContentRepository  extends JpaRepository<Content, Long> {
-}
--- src/main/java/tn/spring/quiz/Repositories/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.spring.quiz.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.spring.quiz.Models.Question;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
