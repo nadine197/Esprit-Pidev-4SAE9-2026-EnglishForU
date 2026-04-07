11cbf009a4e671ff9cbe2d99dcc4154d82b4d91b
nadine197 <nadine.razki@esprit.tn>
2026-04-07 14:56:26 +0100

    Remove backend folder from CourseManagement branch

--- backend/package_service/src/main/java/tn/spring/packagee/Enum/TargetType.java
+++
@@ -1,7 +0,0 @@
-package tn.spring.packagee.Enum;
-
-public enum TargetType {
-    PACKAGE,
-    EVENT,
-    WORKSHOP
-}
--- backend/package_service/src/main/java/tn/spring/packagee/Enum/TransactionStatus.java
+++
@@ -1,7 +0,0 @@
-package tn.spring.packagee.Enum;
-
-public enum TransactionStatus {
-    INITIATED,
-    CONFIRMED,
-    REJECTED
-}
