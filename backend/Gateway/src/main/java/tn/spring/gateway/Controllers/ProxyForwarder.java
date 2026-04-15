0f0bdd14cd4d689fc3e0b62e7d9507890182acc0
nadine197 <nadine.razki@esprit.tn>
2026-04-15 16:14:58 +0100

    add JWT

--- backend/Gateway/src/main/java/tn/spring/gateway/Controllers/ProxyForwarder.java
+++ backend/Gateway/src/main/java/tn/spring/gateway/Controllers/ProxyForwarder.java
@@ -15,46 +15,14 @@ public class ProxyForwarder {
     public ProxyForwarder(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
-    public ResponseEntity<byte[]> forwardBytes(String url,
-                                               HttpMethod method,
-                                               Object body,
-                                               HttpServletRequest req) {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        String auth = req.getHeader(HttpHeaders.AUTHORIZATION);
-        String userId = (String) req.getAttribute("userId");
-        String role = (String) req.getAttribute("role");
-        System.out.println("from proxy " + role + userId);
-
-        if (userId != null) headers.set("X-User-Id", userId);
-        if (role != null) headers.set("X-User-Role", role);
-        if (auth != null) headers.set(HttpHeaders.AUTHORIZATION, auth);
-
-        String accept = req.getHeader(HttpHeaders.ACCEPT);
-        if (accept != null) headers.set(HttpHeaders.ACCEPT, accept);
-
-        HttpEntity<Object> entity = new HttpEntity<>(body, headers);
-
-        ResponseEntity<byte[]> response =
-                restTemplate.exchange(url, method, entity, byte[].class);
-
-        HttpHeaders out = new HttpHeaders();
-        out.putAll(response.getHeaders());
-        return new ResponseEntity<>(response.getBody(), out, response.getStatusCode());
-    }
     public ResponseEntity<String> forward(String url,
                                           HttpMethod method,
                                           Object body,
                                           HttpServletRequest req) {
 
         HttpHeaders headers = new HttpHeaders();
-        String userId = (String) req.getAttribute("userId");
-        String role = (String) req.getAttribute("role");
-        System.out.println("forward  proxy " + role + userId);
 
-        if (userId != null) headers.set("X-User-Id", userId);
-        if (role != null) headers.set("X-User-Role", role);
         String auth = req.getHeader(HttpHeaders.AUTHORIZATION);
         if (auth != null) headers.set(HttpHeaders.AUTHORIZATION, auth);
 
