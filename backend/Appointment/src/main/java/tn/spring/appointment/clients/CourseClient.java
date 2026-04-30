fd5f259ac6c18b95ba090b81176c981d149d4d95
khalilsahnoun123 <sahnoun.khalil78@gmail.com>
2026-04-30 18:05:08 +0100

    fix pakcage path in course client

--- backend/Appointment/src/main/java/tn/spring/appointment/clients/CourseClient.java
+++ backend/Appointment/src/main/java/tn/spring/appointment/clients/CourseClient.java
@@ -1,9 +1,8 @@
-package tn.spring.appointment.clients;
+package tn.spring.appointment.Clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import java.util.List;
 
 // Remplace 8082 par le port REEL de ton microservice Course !
 @FeignClient(name = "COURSE-SERVICE", url = "http://localhost:8082/api/study-groups")
