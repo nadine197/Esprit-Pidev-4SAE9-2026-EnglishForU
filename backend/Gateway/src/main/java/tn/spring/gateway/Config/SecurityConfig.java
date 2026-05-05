a78e305773dd75259437a6fc9a58b875d9601389
khalilsahnoun123 <sahnoun.khalil78@gmail.com>
2026-05-05 21:59:00 +0100

    permit container front acces gateway permit *

--- backend/Gateway/src/main/java/tn/spring/gateway/Config/SecurityConfig.java
+++ backend/Gateway/src/main/java/tn/spring/gateway/Config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "PATCH","POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
